f7fb6f73c9efea3c475de9d95e73856aa84fd1f5
Амирянов Айназ <aamiryanovv@gmail.com>
2025-05-23 13:38:14 +0500

    ref url

--- script/redirectToAuth.js
+++ script/redirectToAuth.js
@@ -11,39 +11,60 @@ document.getElementById("loginBtn").addEventListener("click", function (event) {
     targetUrl = "https://app.harmex.ru/auth";
   }
 
-  window.open(targetUrl, '_blank');
-
+  window.open(targetUrl, "_blank");
 });
-document.getElementById("loginBtnWeb").addEventListener("click", function (event) {
-  event.preventDefault(); // Отключаем стандартное поведение ссылки
+document
+  .getElementById("loginBtnWeb")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Отключаем стандартное поведение ссылки
 
-  const referralCode = localStorage.getItem("referralCode");
-  let targetUrl = "https://app.harmex.ru";
+    const referralCode = localStorage.getItem("referralCode");
+    let targetUrl = "https://app.harmex.ru";
 
-  if (referralCode) {
-    localStorage.removeItem("referralCode");
-    targetUrl = `https://app.harmex.ru/auth?ref=${referralCode}`;
-  } else {
-    targetUrl = "https://app.harmex.ru/auth";
-  }
+    if (referralCode) {
+      localStorage.removeItem("referralCode");
+      targetUrl = `https://app.harmex.ru/auth?ref=${referralCode}`;
+    } else {
+      targetUrl = "https://app.harmex.ru/auth";
+    }
 
-window.open(targetUrl, '_blank');
+    window.open(targetUrl, "_blank");
+  });
 
-});
+document
+  .getElementById("registerBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Отключаем стандартное поведение ссылки
 
-document.getElementById("registerBtn").addEventListener("click", function (event) {
-  event.preventDefault(); // Отключаем стандартное поведение ссылки
+    const referralCode = localStorage.getItem("referralCode");
+    let targetUrl = "https://app.harmex.ru";
 
-  const referralCode = localStorage.getItem("referralCode");
-  let targetUrl = "https://app.harmex.ru";
+    if (referralCode) {
+      localStorage.removeItem("referralCode");
+      targetUrl = `https://app.harmex.ru/register?ref=${referralCode}`;
+    } else {
+      targetUrl =
+        "https://app.harmex.ru/register?ref=7917146c-ef4c-4b05-977c-1be2b73721b7";
+    }
 
-  if (referralCode) {
-    localStorage.removeItem("referralCode");
-    targetUrl = `https://app.harmex.ru/register?ref=${referralCode}`;
-  } else {
-    targetUrl = "https://app.harmex.ru/register?ref=7917146c-ef4c-4b05-977c-1be2b73721b7";
-  }
+    window.open(targetUrl, "_blank");
+  });
 
- window.open(targetUrl, '_blank');
+document.querySelectorAll(".refBtn").forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    console.log("click");
+    event.preventDefault(); // Отключаем стандартное поведение ссылки
 
+    const referralCode = localStorage.getItem("referralCode");
+    let targetUrl = "https://app.harmex.ru";
+
+    if (referralCode) {
+      localStorage.removeItem("referralCode");
+      targetUrl = `https://app.harmex.ru/auth?ref=${referralCode}`;
+    } else {
+      targetUrl = "https://app.harmex.ru/auth";
+    }
+
+    window.open(targetUrl, "_blank");
+  });
 });
